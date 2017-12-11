32cf9186c9364be5c948bc1169252ba828778acf
Alfred Moore <ak_moore@live.com>
2017-12-11 09:29:16 -0600

    Add ability to create tweets. Setup tweet show view.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,24 @@
 class TweetsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :get_tweet, only: [:edit, :show]
+
   def index
     
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    # @tweet.user_id = current_user.id #=> another, more secure way of assigning the user to a tweet, by passing user interference
+    if @tweet.save
+      redirect_to @tweet, notice: 'Successfully saved tweet'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -11,4 +26,15 @@ class TweetsController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+  
+  def get_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
 end
